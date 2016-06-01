c7e1f406e922b0990671462d64426b761d016649
ajkolean <ajkolean@umich.edu>
2016-06-01 19:45:06 -0400

    Memory Game
    Match all cards of the same type to win!

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCard : MonoBehaviour {
+	[SerializeField] private GameObject cardBack;
+	[SerializeField] private SceneController controller;
+
+	private int _id;
+	public int id {
+		get { return _id; }
+	}
+
+	public void SetCard(int id, Sprite image) {
+		Debug.Log (id);
+		_id = id;
+		GetComponent<SpriteRenderer>().sprite = image;
+	}
+
+	public void OnMouseDown() {
+		if (cardBack.activeSelf && controller.canReveal) {
+			cardBack.SetActive (false);
+			controller.CardRevealed (this);
+		}
+	}
+
+	public void Unreveal() {
+		cardBack.SetActive (true);
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
